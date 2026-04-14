f0c192a0b13bedc02fd465375f75d7b81f52bfc7
Jamal Sinani <jamalsinani@gmail.com>
2026-04-14 21:08:28 +0400

    تعديل جديد

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,25 @@
-import Flutter
 import UIKit
+import Flutter
+import Firebase
+import FirebaseMessaging
+import UserNotifications
 
 @main
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, UNUserNotificationCenterDelegate {
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
+    FirebaseApp.configure()
+
+    UNUserNotificationCenter.current().delegate = self
+
+    application.registerForRemoteNotifications()
+
     GeneratedPluginRegistrant.register(with: self)
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
